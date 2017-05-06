8022aac995664c3243b19cb1da2d43801c562431
mybookinglife <baksergei84@gmail.com>
2017-05-06 15:21:43 +0300

    Макет

--- app/src/main/java/com/mybooking/mybookingmobile/Desktop.java
+++ app/src/main/java/com/mybooking/mybookingmobile/Desktop.java
@@ -1,13 +1,41 @@
 package com.mybooking.mybookingmobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Desktop extends AppCompatActivity {
+public class Desktop extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnNewBooking;
+    Button btnListBookings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_desktop);
+
+        btnNewBooking = (Button) findViewById(R.id.btnNewBooking);
+        btnListBookings = (Button) findViewById(R.id.btnListBookings);
+
+        btnNewBooking.setOnClickListener(this);
+        btnListBookings.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnNewBooking:
+                Intent intentNewBooking = new Intent(this, NewBooking.class);
+                startActivity(intentNewBooking);
+                break;
+            case R.id.btnListBookings:
+                Intent intentListBookings = new Intent(this, ListBookings.class);
+                startActivity(intentListBookings);
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/mybooking/mybookingmobile/NewBooking.java
@@ -0,0 +1,13 @@
+package com.mybooking.mybookingmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewBooking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_booking);
+    }
+}
